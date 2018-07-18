41d1d8a68dca1665841b5cb917654107bb4ab354
wysss <1243876958@qq.com>
2018-07-18 09:05:27 +0800

    基于Sping Boot搭建的博客

---
+++ blogone/src/main/java/com/example/blogone/BlogoneApplication.java
@@ -0,0 +1,12 @@
+package com.example.blogone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogoneApplication.class, args);
+    }
+}
